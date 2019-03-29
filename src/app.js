973fd318a3c3c1c9eb13cbac63ce44a9c9d71da8
guenther <email@suepergue.de>
2019-03-29 21:55:09 +0100

    rough implementation with markov-chain-chat

--- src/app.js
+++ src/app.js
@@ -2,15 +2,36 @@
 export {};
 import * as bot from "bbot";
 import { asimovDirective } from "./rules/directives";
+import path from "path";
+import markovChainChat from "markov-chain-chat";
+
+const myMarkovChain = new markovChainChat(
+  path.resolve(__dirname, "../samples/chatlog_simple.txt")
+);
 
 asimovDirective();
 
+// bot.global.text(/say hello to (.*)/i, b => {
+//   b.respond("Hello " + b.match[1]);
+// });
+
+// bot.global.text(
+//   {
+//     contains: ["hello"]
+//   },
+//   b => b.respond("Hello 👋"),
+//   {
+//     id: "hello-bots"
+//   }
+// );
+
 bot.global.text(
-  {
-    contains: ["hi", "hello"]
+  /(.*)/,
+  b => {
+    const markovResponse = myMarkovChain.getMessage(b.match[1]);
+    if (markovResponse) b.respond(markovResponse);
   },
-  b => b.respond("Hello 👋"),
   {
-    id: "hello-bots"
+    id: "markov-response"
   }
 );
